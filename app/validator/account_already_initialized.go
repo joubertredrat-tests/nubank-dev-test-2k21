18e1e84cb47ced3949c8acd16707fbdb65fc1670
Joubert RedRat <eu+github@redrat.com.br>
2021-05-02 22:14:56 -0300

    add account already initialized validator and tests

---
+++ app/validator/account_already_initialized.go
@@ -0,0 +1,22 @@
+package validator
+
+import "dev-test/nubank-dev-test-2k21/app/entity"
+
+type AccountAlreadyInitializedValidator struct{}
+
+func NewAccountAlreadyInitializedValidator() *AccountAlreadyInitializedValidator {
+	return &AccountAlreadyInitializedValidator{}
+}
+
+func (v *AccountAlreadyInitializedValidator) IsBreakNextCheck() bool {
+	return true
+}
+
+func (v *AccountAlreadyInitializedValidator) GetViolation(account entity.Account, transaction entity.Transaction) *entity.Violation {
+	if account.IsInitialized() {
+		violation := entity.NewViolationAccountAlreadyInitialized()
+		return &violation
+	}
+
+	return nil
+}
