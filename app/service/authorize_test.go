b5b1cbd2b68356b0b548cf15fa7c94acc559cb83
Joubert RedRat <eu+github@redrat.com.br>
2021-05-04 13:34:55 -0300

    finish tests for service

--- app/service/authorize_test.go
+++ app/service/authorize_test.go
@@ -209,6 +209,304 @@ func TestAuthorizeService(t *testing.T) {
 				return operations
 			},
 		},
+		{
+			name: "Test handle operations with violations for account already created",
+			authorizeService: service.NewAuthorizeService(
+				validator.NewManager(
+					[]validator.ValidatorInterface{
+						validator.NewCardLimitValidator(),
+						validator.NewHighTransactionsValidator(3, 120),
+						validator.NewDoubleTransactionValidator(120),
+					},
+				),
+			),
+			getInputOperations: func() input.Operations {
+				operations := input.NewOperations()
+				operations.AddLine(input.AccountLine{
+					Account: struct {
+						ActiveCard     bool `json:"active-card"`
+						AvailableLimit uint `json:"available-limit"`
+					}{
+						ActiveCard:     true,
+						AvailableLimit: 120,
+					},
+				})
+				operations.AddLine(input.TransactionLine{
+					Transaction: struct {
+						Merchant string    `json:"merchant"`
+						Amount   uint      `json:"amount"`
+						Time     time.Time `json:"time"`
+					}{
+						Merchant: "Burger King",
+						Amount:   20,
+						Time:     helper.GetTimeFromString("2021-04-20T19:25:00.000Z"),
+					},
+				})
+				operations.AddLine(input.TransactionLine{
+					Transaction: struct {
+						Merchant string    `json:"merchant"`
+						Amount   uint      `json:"amount"`
+						Time     time.Time `json:"time"`
+					}{
+						Merchant: "Habib's",
+						Amount:   10,
+						Time:     helper.GetTimeFromString("2021-04-20T19:42:00.000Z"),
+					},
+				})
+				operations.AddLine(input.AccountLine{
+					Account: struct {
+						ActiveCard     bool `json:"active-card"`
+						AvailableLimit uint `json:"available-limit"`
+					}{
+						ActiveCard:     true,
+						AvailableLimit: 120,
+					},
+				})
+				operations.AddLine(input.TransactionLine{
+					Transaction: struct {
+						Merchant string    `json:"merchant"`
+						Amount   uint      `json:"amount"`
+						Time     time.Time `json:"time"`
+					}{
+						Merchant: "Bob's",
+						Amount:   15,
+						Time:     helper.GetTimeFromString("2021-04-21T07:04:00.000Z"),
+					},
+				})
+				operations.AddLine(input.TransactionLine{
+					Transaction: struct {
+						Merchant string    `json:"merchant"`
+						Amount   uint      `json:"amount"`
+						Time     time.Time `json:"time"`
+					}{
+						Merchant: "Subway",
+						Amount:   30,
+						Time:     helper.GetTimeFromString("2021-04-21T07:15:00.000Z"),
+					},
+				})
+
+				return operations
+			},
+			getOperationsExpected: func() entity.Operations {
+				operations := entity.NewOperations()
+				operations.RegisterEvent(
+					entity.NewAccount(true, 120),
+					entity.NewViolationsEmpty(),
+				)
+				operations.RegisterEvent(
+					entity.NewAccount(true, 100),
+					entity.NewViolationsEmpty(),
+				)
+				operations.RegisterEvent(
+					entity.NewAccount(true, 90),
+					entity.NewViolationsEmpty(),
+				)
+				operations.RegisterViolationEvent(
+					entity.NewAccount(true, 90),
+					entity.NewViolationAccountAlreadyInitialized(),
+				)
+				operations.RegisterEvent(
+					entity.NewAccount(true, 75),
+					entity.NewViolationsEmpty(),
+				)
+				operations.RegisterEvent(
+					entity.NewAccount(true, 45),
+					entity.NewViolationsEmpty(),
+				)
+
+				return operations
+			},
+		},
+		{
+			name: "Test handle operations with multiple violations",
+			authorizeService: service.NewAuthorizeService(
+				validator.NewManager(
+					[]validator.ValidatorInterface{
+						validator.NewCardLimitValidator(),
+						validator.NewHighTransactionsValidator(3, 120),
+						validator.NewDoubleTransactionValidator(120),
+					},
+				),
+			),
+			getInputOperations: func() input.Operations {
+				operations := input.NewOperations()
+				operations.AddLine(input.TransactionLine{
+					Transaction: struct {
+						Merchant string    `json:"merchant"`
+						Amount   uint      `json:"amount"`
+						Time     time.Time `json:"time"`
+					}{
+						Merchant: "Burger King",
+						Amount:   20,
+						Time:     helper.GetTimeFromString("2021-04-20T19:25:00.000Z"),
+					},
+				})
+				operations.AddLine(input.AccountLine{
+					Account: struct {
+						ActiveCard     bool `json:"active-card"`
+						AvailableLimit uint `json:"available-limit"`
+					}{
+						ActiveCard:     true,
+						AvailableLimit: 120,
+					},
+				})
+				operations.AddLine(input.TransactionLine{
+					Transaction: struct {
+						Merchant string    `json:"merchant"`
+						Amount   uint      `json:"amount"`
+						Time     time.Time `json:"time"`
+					}{
+						Merchant: "Habib's",
+						Amount:   10,
+						Time:     helper.GetTimeFromString("2021-04-20T19:42:00.000Z"),
+					},
+				})
+				operations.AddLine(input.TransactionLine{
+					Transaction: struct {
+						Merchant string    `json:"merchant"`
+						Amount   uint      `json:"amount"`
+						Time     time.Time `json:"time"`
+					}{
+						Merchant: "Bob's",
+						Amount:   15,
+						Time:     helper.GetTimeFromString("2021-04-21T07:04:00.000Z"),
+					},
+				})
+				operations.AddLine(input.AccountLine{
+					Account: struct {
+						ActiveCard     bool `json:"active-card"`
+						AvailableLimit uint `json:"available-limit"`
+					}{
+						ActiveCard:     true,
+						AvailableLimit: 120,
+					},
+				})
+				operations.AddLine(input.TransactionLine{
+					Transaction: struct {
+						Merchant string    `json:"merchant"`
+						Amount   uint      `json:"amount"`
+						Time     time.Time `json:"time"`
+					}{
+						Merchant: "Subway",
+						Amount:   60,
+						Time:     helper.GetTimeFromString("2021-04-21T07:15:00.000Z"),
+					},
+				})
+				operations.AddLine(input.TransactionLine{
+					Transaction: struct {
+						Merchant string    `json:"merchant"`
+						Amount   uint      `json:"amount"`
+						Time     time.Time `json:"time"`
+					}{
+						Merchant: "Subway",
+						Amount:   60,
+						Time:     helper.GetTimeFromString("2021-04-21T07:15:00.000Z"),
+					},
+				})
+				operations.AddLine(input.TransactionLine{
+					Transaction: struct {
+						Merchant string    `json:"merchant"`
+						Amount   uint      `json:"amount"`
+						Time     time.Time `json:"time"`
+					}{
+						Merchant: "Yogoberry",
+						Amount:   15,
+						Time:     helper.GetTimeFromString("2021-04-21T14:37:00.000Z"),
+					},
+				})
+				operations.AddLine(input.TransactionLine{
+					Transaction: struct {
+						Merchant string    `json:"merchant"`
+						Amount   uint      `json:"amount"`
+						Time     time.Time `json:"time"`
+					}{
+						Merchant: "Starbucks",
+						Amount:   5,
+						Time:     helper.GetTimeFromString("2021-04-21T16:55:00.000Z"),
+					},
+				})
+				operations.AddLine(input.TransactionLine{
+					Transaction: struct {
+						Merchant string    `json:"merchant"`
+						Amount   uint      `json:"amount"`
+						Time     time.Time `json:"time"`
+					}{
+						Merchant: "Mcdonald's",
+						Amount:   10,
+						Time:     helper.GetTimeFromString("2021-04-21T16:56:00.000Z"),
+					},
+				})
+				operations.AddLine(input.TransactionLine{
+					Transaction: struct {
+						Merchant string    `json:"merchant"`
+						Amount   uint      `json:"amount"`
+						Time     time.Time `json:"time"`
+					}{
+						Merchant: "Mcdonald's",
+						Amount:   10,
+						Time:     helper.GetTimeFromString("2021-04-21T16:56:00.000Z"),
+					},
+				})
+
+				return operations
+			},
+			getOperationsExpected: func() entity.Operations {
+				operations := entity.NewOperations()
+				operations.RegisterViolationEvent(
+					entity.NewAccountEmpty(),
+					entity.NewViolationAccountNotInitialized(),
+				)
+				operations.RegisterEvent(
+					entity.NewAccount(true, 120),
+					entity.NewViolationsEmpty(),
+				)
+				operations.RegisterEvent(
+					entity.NewAccount(true, 110),
+					entity.NewViolationsEmpty(),
+				)
+				operations.RegisterEvent(
+					entity.NewAccount(true, 95),
+					entity.NewViolationsEmpty(),
+				)
+				operations.RegisterViolationEvent(
+					entity.NewAccount(true, 95),
+					entity.NewViolationAccountAlreadyInitialized(),
+				)
+				operations.RegisterEvent(
+					entity.NewAccount(true, 35),
+					entity.NewViolationsEmpty(),
+				)
+				operations.RegisterEvent(
+					entity.NewAccount(true, 35),
+					[]*entity.Violation{
+						entity.NewViolationInsufficientLimit(),
+						entity.NewViolationDoubleTransaction(),
+					},
+				)
+				operations.RegisterEvent(
+					entity.NewAccount(true, 20),
+					entity.NewViolationsEmpty(),
+				)
+				operations.RegisterEvent(
+					entity.NewAccount(true, 15),
+					entity.NewViolationsEmpty(),
+				)
+				operations.RegisterEvent(
+					entity.NewAccount(true, 5),
+					entity.NewViolationsEmpty(),
+				)
+				operations.RegisterEvent(
+					entity.NewAccount(true, 5),
+					[]*entity.Violation{
+						entity.NewViolationInsufficientLimit(),
+						entity.NewViolationHighFrequencySmallInterval(),
+						entity.NewViolationDoubleTransaction(),
+					},
+				)
+
+				return operations
+			},
+		},
 	}
 
 	for _, test := range tests {
